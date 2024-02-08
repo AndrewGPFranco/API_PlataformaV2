e46b5ced1a861f2708789458b5f42bb465d59b5e
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-07 21:25:22 -0300

    first commit

---
+++ src/test/java/com/gpf/ti/TiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpf.ti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
