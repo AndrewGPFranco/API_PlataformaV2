4d5981ebff221679134d2900efc56fb6af6ace36
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-13 18:48:41 -0300

    fix: Retorno do método de login para casos de usuario ou senha invalidos

--- src/main/java/com/gpf/ti/controller/AutenticacaoController.java
+++ src/main/java/com/gpf/ti/controller/AutenticacaoController.java
@@ -6,13 +6,19 @@ import com.gpf.ti.model.Usuario;
 import com.gpf.ti.services.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/login")
@@ -26,10 +32,16 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticaoDto dados) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(authenticationToken);
+        try {
+            var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+            var authentication = manager.authenticate(authenticationToken);
 
-        var tokenJwt = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-        return ResponseEntity.ok(new DadosTokenJwtDto(tokenJwt));
+            var tokenJwt = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+            return ResponseEntity.ok(new DadosTokenJwtDto(tokenJwt));
+        } catch (AuthenticationException e) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("Erro", e.getMessage());
+            return new ResponseEntity(response, HttpStatus.FORBIDDEN);
+        }
     }
 }
