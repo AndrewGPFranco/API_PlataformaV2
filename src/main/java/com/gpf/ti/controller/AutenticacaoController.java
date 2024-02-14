04a42bd927cf2d3aca489c6a8796839da82b5aa8
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-13 22:12:39 -0300

    style: identação do código

--- src/main/java/com/gpf/ti/controller/AutenticacaoController.java
+++ src/main/java/com/gpf/ti/controller/AutenticacaoController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/login")
