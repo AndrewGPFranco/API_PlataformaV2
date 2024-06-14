51e7b4ed0ca09b9493443ade70b71767961f595c
AndrewGPSilva <andrewgomes1328@gmail.com>
2024-06-13 22:13:33 -0300

    tornando telefone atributo unico

--- src/main/java/com/gpf/ti/dtos/usuario/UserDto.java
+++ src/main/java/com/gpf/ti/dtos/usuario/UserDto.java
@@ -1,8 +1,6 @@
 package com.gpf.ti.dtos.usuario;
 
 import com.gpf.ti.enums.GeneroType;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
